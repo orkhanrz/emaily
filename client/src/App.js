dcb876f51f0d990bbc220cf5c61fc21297de2dec
Orkhan Rzali <orkhan.rz8@gmail.com>
2024-12-02 08:44:46 +0400

    client side initialized

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello world!</h1>
+      <a href="/auth/google">Login with Google</a>
+    </div>
+  );
+}
+
+export default App;

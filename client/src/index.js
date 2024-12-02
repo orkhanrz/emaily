6ec436076d1c785ca03666f2252a9549f5a9b94e
orkhan.rz <orkhan.rz@bestcomp.net>
2024-12-02 17:58:59 +0400

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
--- src/services/passport.js
+++ src/services/passport.js
@@ -5,37 +5,37 @@ const User = require("../models/user");
 
 // User here is coming from google strategy's callback function.
 passport.serializeUser((user, done) => {
-	done(null, user.id);
+  done(null, user.id);
 });
 
 // Take user's id from serializeUser and turn into into a user.
 passport.deserializeUser(async (id, done) => {
-	const user = await User.findById(id);
+  const user = await User.findById(id);
 
-	if (!user) {
-		return done(true, null);
-	}
+  if (!user) {
+    return done(true, null);
+  }
 
-	done(null, user);
+  done(null, user);
 });
 
 passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientId,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: "/auth/google/callback",
-			proxy: true,
-		},
-		async (accessToken, refreshToken, profile, done) => {
-			const existingUser = await User.findOne({ googleId: profile.id });
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    async (_accessToken, _refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
 
-			if (existingUser) {
-				return done(null, existingUser);
-			}
-      
-			const newUser = await new User({ googleId: profile.id }).save();
-			done(null, newUser);
-		}
-	)
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const newUser = await new User({ googleId: profile.id }).save();
+      done(null, newUser);
+    }
+  )
 );
